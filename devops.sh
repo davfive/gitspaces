30b68dc85f1084e8988561d00469fd100aa2ea55
David May <david.may@efi.com>
2024-03-18 17:53:10 -0700

    fixing devops.sh

--- devops.sh
+++ devops.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
+#------------------------------------------------------------
+VERSION=`git describe --long --tags --dirty`
+if [[ $? -ne 0 -o -z "${version}" ]]; then
+    echo "Failed to get version from 'git describe --long --tags --dirty'"
+    exit 1
+fi
+#------------------------------------------------------------
 
-LDFLAGS="-X main.Version=$version"
+function check_version() {
+    if [[ "$VERSION" = *-dirty ]]; then
+        echo "Cannot publish with modified (dirty) working directory: "$version
+        echo "Please commit or stash your changes"
+        exit 1
+    elif ! [[ "$VERSION" = *-0-* ]]; then
+        echo "Cannot publish tag with later commits: "$version
+        echo "Please create/push a new tag"
+        exit 1
+    fi
+}
+
+function gorun() {
+    (set -x; "$0" -ldflags \"-X main.Version=$VERSION\")
+}
 
 case "$1" in
     "build")
-        version=`git describe --long --tags --dirty`
-        (set -x; go build -ldflags "-X main.Version=$version" -o build/gitspaces)
+        gorun go build -o build/gitspaces
         ;;
     "install")
-        version=`git describe --long --tags --dirty`
-        (set -x; go install -ldflags "-X main.Version=$version")
+        gorun install
         ;;
     "publish")
-        version=`git describe --long --tags --dirty 2>/dev/null`
-        if [ $? -ne 0 ]; then
-            echo "Failed to get version from 'git describe --long --tags --dirty'"
-            exit 1
-        elif [[ "$version" = *-dirty ]]; then
-            echo "Cannot publish with modified (dirty) working directory: "$version
-            echo "Please commit or stash your changes"
-            exit 1
-        elif ! [[ "$version" = *-0-* ]]; then
-            echo "Cannot publish tag with later commits: "$version
-            echo "Please create/push a new tag"
-            exit 1
-        fi
-        
-        echo GOPROXY=proxy.golang.org go list -m github.com/davfive/gitspaces/v2@ -ldflags \"-X main.Version=$version\"
+        check_version
+        GOPROXY=proxy.golang.org gorun list -m github.com/davfive/gitspaces/v2@$VERSION
         exit 1
         ;;
     *)
