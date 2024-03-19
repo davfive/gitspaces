ebad26f8c789d364f17d2a67a789c06c85003f36
David May <david.may@efi.com>
2024-03-18 18:04:38 -0700

    fixing devops.sh

--- devops.sh
+++ devops.sh
@@ -11,17 +11,17 @@ function check_version() {
     local branch=$(git branch --show-current)
     local majver=$(echo $VERSION | cut -d. -f1)
 
-    if [[ "$VERSION" = *-dirty ]]; then
+    if ! [[ "$branch" = $majver.* ]]; then
+        echo "Cannot publish $VERSION from branch $branch"
+        echo "Please check $VERSION version release branch: $majver"
+        exit 1
+    elif [[ "$VERSION" = *-dirty ]]; then
         echo "Cannot publish with modified (dirty) working directory: $VERSION"
         echo "Please commit or stash your changes"
         exit 1
     elif ! [[ "$VERSION" = *-0-* ]]; then
         echo "Cannot publish tag with later commits: $VERSION"
         echo "Please create/push a new tag"
-        exit 1
-    elif ! [[ "$branch" = $majver.* ]]; then
-        echo "Cannot publish $VERSION from branch $branch"
-        echo "Please check $VERSION version release branch: $majver"
         exit 1
     fi
 }
