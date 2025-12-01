1f621f6ea6d5c75158da59862628d9d6aa5f4dbf
davfive <davfive@gmail.com>
2025-11-30 20:23:23 -0800

    ci: fix bad coverage_args array usage. just update $@

--- scripts/ci/run-tests.sh
+++ scripts/ci/run-tests.sh
@@ -16,10 +16,9 @@ pip install -e .[dev]
 
 
 # If UPLOAD_COVERAGE or TEST_COVERAGE_FILE is set, add coverage options
-COVERAGE_ARGS=()
 if [[ "${UPLOAD_COVERAGE:-}" == "true" || -n "${TEST_COVERAGE_FILE:-}" ]]; then
   COV_FILE="${TEST_COVERAGE_FILE:-coverage-$(python -c 'import sys; print(\"%d.%d\" % sys.version_info[:2])').xml}"
-  COVERAGE_ARGS+=(--cov=src/gitspaces --cov-report=xml:"$COV_FILE" --cov-report=term)
+  set -- "$@" --cov=src/gitspaces --cov-report=xml:"$COV_FILE" --cov-report=term
 fi
 
-pytest "$@" "${COVERAGE_ARGS[@]}"
+pytest "$@"
